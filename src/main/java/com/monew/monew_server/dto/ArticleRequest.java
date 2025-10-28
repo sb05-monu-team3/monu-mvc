342456d56598ba16c4bf82178c963c27590c3247
jymin0 <alswodud119@naver.com>
2025-10-28 10:26:46 +0900

    refactor: 불필요 주석 제거 #12

--- src/main/java/com/monew/monew_server/dto/ArticleRequest.java
+++ src/main/java/com/monew/monew_server/dto/ArticleRequest.java
@@ -8,13 +8,13 @@ import com.monew.monew_server.domain.article.entity.ArticleSortType;
 import com.monew.monew_server.domain.article.entity.ArticleSource;
 
 public record ArticleRequest(
-	String keyword,             // 제목 또는 요약 검색어
-	List<UUID> interestIds,     // 관심사 ID 목록
-	ArticleSource source,       // 출처 필터링
-	LocalDate date,             // 날짜 필터링
-	String cursor,              // 커서 기반 페이지네이션을 위한 다음 커서 값
-	ArticleSortType sortBy,     // 정렬 기준 (날짜, 댓글 수, 조회 수)
-	Integer size,                // 페이지 크기
+	String keyword,
+	List<UUID> interestIds,
+	ArticleSource source,
+	LocalDate date,
+	String cursor,
+	ArticleSortType sortBy,
+	Integer size,
 	String nextAfter
 ) {
 }
