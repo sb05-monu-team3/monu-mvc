6687d54697c7c2b57b27ef11d92a79b8f2595313
backKim1024 <dydgml7313@naver.com>
2025-10-29 15:02:32 +0900

    feat(User): Add UserDto for user response data
    Body:
    사용자 정보를 클라이언트에게 응답하기 위한 DTO 클래스 UserDto를 추가
    위 DTO는 사용자 ID, email, nickname, 생성 시간을 포함하여, 민감 정보를 제외한
    데이터를 전송에 사용됨

--- src/main/java/com/monew/monew_server/domain/user/dto/UserDto.java
+++ src/main/java/com/monew/monew_server/domain/user/dto/UserDto.java
@@ -1,15 +1,22 @@
 package com.monew.monew_server.domain.user.dto;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
-@Setter
 @Builder
 public class UserDto {
 
-
-
+	private UUID id;
+	private String email;
+	private String nickname;
+	private OffsetDateTime createdAt;
 
 }
