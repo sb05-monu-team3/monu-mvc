352694707a2f85c76736aab5fb51c9af64530852
backKim1024 <dydgml7313@naver.com>
2025-10-29 19:14:01 +0900

    feat(User): Add UserRepository for register
    <body>
    회원가입을 위한 UserRepository 구현
    - email 중복 체크
    - nickname 중복 체크
    
    # Conflicts:
    #	src/main/java/com/monew/monew_server/domain/user/repository/UserRepository.java

--- src/main/java/com/monew/monew_server/domain/user/repository/UserRepository.java
+++ src/main/java/com/monew/monew_server/domain/user/repository/UserRepository.java
@@ -1,8 +1,23 @@
 package com.monew.monew_server.domain.user.repository;
 
-import com.monew.monew_server.domain.user.entity.User;
+import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.monew.monew_server.domain.user.entity.User;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+	// 회원가입
+	boolean existsByEmail(String email);
+	boolean existsByNickname(String nickname);
+
+	// 로그인
+	Optional<User> findByEmail(String email);
+
+
+
 }
