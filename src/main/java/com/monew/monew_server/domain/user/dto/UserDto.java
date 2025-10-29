6115815919f2da799b9f0848dc3c7fcaf0e011da
backKim1024 <dydgml7313@naver.com>
2025-10-29 14:42:35 +0900

    temp: save local changes before rebase

---
+++ src/main/java/com/monew/monew_server/domain/user/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.monew.monew_server.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserDto {
+
+
+
+
+}
