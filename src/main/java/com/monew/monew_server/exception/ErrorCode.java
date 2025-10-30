9a102568a16e52bb6f8933c58d031aae526f82b8
BakJiSeok <commicat2@gmail.com>
2025-10-30 16:28:29 +0900

    chore: conflict 해결

--- src/main/java/com/monew/monew_server/exception/ErrorCode.java
+++ src/main/java/com/monew/monew_server/exception/ErrorCode.java
@@ -7,10 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-	ARTICLE_NOT_FOUND("요청하신 게시글을 찾을 수 없습니다."),
-	INTEREST_NAME_DUPLICATION("유사한 이름의 관심사가 이미 존재합니다."),
 	INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다."),
-	INVALID_REQUEST("잘못된 요청입니다.");
+	INVALID_REQUEST("잘못된 요청입니다."),
+	ARTICLE_NOT_FOUND("요청하신 게시글을 찾을 수 없습니다."),
+	INTEREST_NAME_DUPLICATION("유사한 이름의 관심사가 이미 존재합니다.");
 
 	private final String message;
 }
