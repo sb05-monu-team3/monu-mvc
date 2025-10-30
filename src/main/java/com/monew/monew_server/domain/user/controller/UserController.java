a02a0458d2759cfd3efb4d506968eaac5ce12a0b
backKim1024 <dydgml7313@naver.com>
2025-10-30 17:14:05 +0900

    feat(User) : 회원 정보의 소프트 삭제, 하드 삭제에 대한 API 구현
    <body>
    - BaseDeletableEntity에 상속되어 회원 정보가 공개되지 않는 상태로 만드는 소프트 삭제 API 구현
    DELETE /api/users/{userId}
    - 소프트 삭제가 완료된 회원만 하드 삭제가 되도록 구현
    DELETE /api/users/{userId}/hard

--- src/main/java/com/monew/monew_server/domain/user/controller/UserController.java
+++ src/main/java/com/monew/monew_server/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.monew.monew_server.domain.user.controller;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,6 @@ import com.monew.monew_server.domain.user.dto.UserDto;
 import com.monew.monew_server.domain.user.dto.UserLoginRequest;
 import com.monew.monew_server.domain.user.dto.UserRegisterRequest;
 import com.monew.monew_server.domain.user.dto.UserUpdateRequset;
-import com.monew.monew_server.domain.user.entity.User;
 import com.monew.monew_server.domain.user.service.UserService;
 
 import jakarta.validation.Valid;
@@ -42,6 +42,18 @@ public class UserController {
 	public ResponseEntity<UserDto> updateNickname(@PathVariable UUID userId, @Valid @RequestBody UserUpdateRequset request) {
 		UserDto userDto = userService.updateNickname(userId, request);
 		return ResponseEntity.ok(userDto);
+	}
+
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<String> deleteUser(@PathVariable UUID userId) {
+		userService.deleteUser(userId);
+		return ResponseEntity.ok("회원이 삭제되었습니다.");
+	}
+
+	@DeleteMapping("/{userId}/hard")
+	public ResponseEntity<String> hardDeleteUser(@PathVariable UUID userId){
+		userService.hardDeleteUser(userId);
+		return ResponseEntity.ok("회원의 데이터가 영구적으로 삭제되었습니다.");
 	}
 
 
