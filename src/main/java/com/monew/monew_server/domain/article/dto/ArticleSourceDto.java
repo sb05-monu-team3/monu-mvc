8d23170f32282a687da08633283c51f899696841
jymin0 <alswodud119@naver.com>
2025-10-30 15:12:14 +0900

    feat: 출처 목록 조회 #27

---
+++ src/main/java/com/monew/monew_server/domain/article/dto/ArticleSourceDto.java
@@ -0,0 +1,10 @@
+package com.monew.monew_server.domain.article.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ArticleSourceDto {
+	private String name;
+}
