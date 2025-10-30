c9bdf544c9931c9e830a77ab02ac5681ed5d5c89
backKim1024 <dydgml7313@naver.com>
2025-10-30 15:09:16 +0900

    feat(User) : UserUpdate를 위한 repository 추가
    <body>
    findByNickname 추가

--- src/main/java/com/monew/monew_server/domain/user/service/UserService.java
+++ src/main/java/com/monew/monew_server/domain/user/service/UserService.java
@@ -1,11 +1,15 @@
 package com.monew.monew_server.domain.user.service;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.monew.monew_server.domain.user.dto.UserDto;
 import com.monew.monew_server.domain.user.dto.UserLoginRequest;
 import com.monew.monew_server.domain.user.dto.UserRegisterRequest;
+import com.monew.monew_server.domain.user.dto.UserUpdateRequset;
 import com.monew.monew_server.domain.user.entity.User;
 import com.monew.monew_server.domain.user.repository.UserRepository;
 
@@ -52,6 +56,37 @@ public class UserService {
 			throw new IllegalArgumentException("비밀번호가 올바르지 않습니다.");
 		}
 
+		return UserDto.builder()
+			.id(user.getId())
+			.email(user.getEmail())
+			.nickname(user.getNickname())
+			.createdAt(user.getCreatedAt().atOffset(java.time.ZoneOffset.UTC))
+			.build();
+	}
+
+	@Transactional
+	public UserDto updateNickname (UUID userId, UserUpdateRequset requset) {
+		// 유저 존재 확인
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+		// 소프트 삭제 여부 확인
+		if (user.getDeletedAt() != null) {
+			throw new IllegalArgumentException("삭제된 사용자입니다.");
+		}
+
+		// 닉네임 중복 확인
+		if (userRepository.existsByNickname(requset.getNickname())) {
+			throw new IllegalArgumentException("이미 존재하는 닉네임 입니다.");
+		}
+
+		// 닉네임 수정
+		user.setNickname(requset.getNickname());
+
+		// 저장
+		userRepository.save(user);
+
+		// 응답 DTO 반환
 		return UserDto.builder()
 			.id(user.getId())
 			.email(user.getEmail())
