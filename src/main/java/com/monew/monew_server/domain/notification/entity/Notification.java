2ae7802f1868d5f2b825dc0d0d37476ca4dff8e4
polodumbo <polosh1208@gmail.com>
2025-10-31 15:18:52 +0900

    fix(notification): enum 대소문자 불일치 문제 해결
    - NotificationResourceTypeConverter 추가

--- src/main/java/com/monew/monew_server/domain/notification/entity/Notification.java
+++ src/main/java/com/monew/monew_server/domain/notification/entity/Notification.java
@@ -4,12 +4,12 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.monew.monew_server.domain.common.BaseUpdatableEntity;
+import com.monew.monew_server.domain.notification.converter.NotificationResourceTypeConverter;
 import com.monew.monew_server.domain.user.entity.User;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -45,7 +45,7 @@ public class Notification extends BaseUpdatableEntity {
 	@Column(columnDefinition = "text", nullable = false)
 	private String content;
 
-	@Enumerated(EnumType.STRING)
+	@Convert(converter = NotificationResourceTypeConverter.class)
 	@Column(nullable = false, length = 50)
 	private NotificationResourceType resourceType;
 
